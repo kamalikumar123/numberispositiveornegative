ee8364db22be354c4c5006a134bf3d10d726e173
kamali <93373518+kamalikumar123@users.noreply.github.com>
2021-12-18 14:48:17 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,11 @@
+public class main{
+public static void main(String[]args){
+number=12.5;
+if(number<0.0)
+System.out.println(number+"is a negative number);
+else if(number>0.0)
+System.out.println(number+"is a positive number);
+else
+System.out.println(number+"is a 0);
+}
+}
